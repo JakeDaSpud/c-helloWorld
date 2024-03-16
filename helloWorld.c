740882cacf4bbd8ad21e5a5bc705d92b3ca54c31
Jake O'Reilly <78006039+JakeDaSpud@users.noreply.github.com>
2024-03-16 01:00:45 +0000

    - Added all code

---
+++ helloWorld.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    printf("Hello, World!\n");
+    return 0;
+}
+
+// To compile the code (before running it), open a terminal / cmd and run:
+// (the .c file is saved as "helloWorld.c", so use the right words!)
+
+// **gcc version**
+// gcc helloWorld.c -o helloWorld
+// Then run: ./helloWorld
